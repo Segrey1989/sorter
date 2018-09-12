547b427a3d2cd3728a8844848e9507aa0e738d0e
Siarhei Kvachonak <segrey1989@tut.by>
2018-09-12 21:58:30 +0300

    sorter: all works except two tests

--- src/index.js
+++ src/index.js
@@ -1,31 +1,49 @@
 class Sorter {
   constructor() {
-    // your implementation
+  this.array = [];
+  this.len = 0;
+ this.comparator;
   }
 
   add(element) {
-    // your implementation
+    this.array[this.len++] = element;
   }
 
   at(index) {
-    // your implementation
+    if(index < this.len)
+    return this.array[index];
   }
 
   get length() {
-    // your implementation
+    return this.len;
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
 
   sort(indices) {
-    // your implementation
-  }
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+   for(var x = 1; x <= (indices.length-1); x++){
+    if(indices[0] > indices[x]){
+    var tmp = indices[0];
+    indices[0] = indices[x];
+    indices[x] = tmp;
 }
 
+var arr = this.array.slice(indices[0], indices[x]+1);
+if(this.comparator)
+arr.sort(this.comparator);
+else
+arr.sort(function(a,b){return a-b;});
+this.array.splice(indices[0],indices[x] -indices[0]+1 , ...arr);
+}
+  }
+
+
+  setComparator(compareFunction) {
+      this.comparator = compareFunction;
+
+}
+}
 module.exports = Sorter;
